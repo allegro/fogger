41364f6babe8c72f57abb6c5b0aa8a407b2b29ea
Pawel Byszewski <pawel.byszewski@ext.allegro.pl>
2015-07-16 20:41:12 +0200

    update build gradle to generate archives with docs and sources; clear javadco in SafeAsyncTask(roboguice)

--- fogger/src/main/java/pl/allegro/fogger/utils/SafeAsyncTask.java
+++ fogger/src/main/java/pl/allegro/fogger/utils/SafeAsyncTask.java
@@ -29,23 +29,6 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
-/**
- * Originally from RoboGuice: https://github.com/roboguice/roboguice/blob/master/roboguice/src/main/java/roboguice/util/SafeAsyncTask.java
- * <p/>
- * A class similar but unrelated to android's {@link android.os.AsyncTask}.
- * <p/>
- * Unlike AsyncTask, this class properly propagates exceptions.
- * <p/>
- * If you're familiar with AsyncTask and are looking for {@link android.os.AsyncTask#doInBackground(Object[])},
- * we've named it {@link #call()} here to conform with java 1.5's {@link java.util.concurrent.Callable} interface.
- * <p/>
- * Current limitations: does not yet handle progress, although it shouldn't be
- * hard to add.
- * <p/>
- * If using your own executor, you must call future() to get a runnable you can execute.
- *
- * @param <ResultT>
- */
 public abstract class SafeAsyncTask<ResultT> implements Callable<ResultT> {
     public static final int DEFAULT_POOL_SIZE = 25;
     protected static final Executor DEFAULT_EXECUTOR = Executors.newFixedThreadPool(DEFAULT_POOL_SIZE);
@@ -55,25 +38,15 @@ public abstract class SafeAsyncTask<ResultT> implements Callable<ResultT> {
     protected FutureTask<Void> future;
 
 
-    /**
-     * Sets executor to Executors.newFixedThreadPool(DEFAULT_POOL_SIZE) and
-     * Handler to new Handler()
-     */
     public SafeAsyncTask() {
         this.executor = DEFAULT_EXECUTOR;
     }
 
-    /**
-     * Sets executor to Executors.newFixedThreadPool(DEFAULT_POOL_SIZE)
-     */
     public SafeAsyncTask(Handler handler) {
         this.handler = handler;
         this.executor = DEFAULT_EXECUTOR;
     }
 
-    /**
-     * Sets Handler to new Handler()
-     */
     public SafeAsyncTask(Executor executor) {
         this.executor = executor;
     }
@@ -122,41 +95,18 @@ public abstract class SafeAsyncTask<ResultT> implements Callable<ResultT> {
         return future.cancel(mayInterruptIfRunning);
     }
 
-    /**
-     * @throws Exception, captured on passed to onException() if present.
-     */
     protected void onPreExecute() throws Exception {
     }
 
-    /**
-     * @param t the result of {@link #call()}
-     * @throws Exception, captured on passed to onException() if present.
-     */
+
     @SuppressWarnings({"UnusedDeclaration"})
     protected void onSuccess(ResultT t) throws Exception {
     }
 
-    /**
-     * Called when the thread has been interrupted, likely because
-     * the task was canceled.
-     * <p/>
-     * By default, calls {@link #onException(Exception)}, but this method
-     * may be overridden to handle interruptions differently than other
-     * exceptions.
-     *
-     * @param e an InterruptedException or InterruptedIOException
-     */
     protected void onInterrupted(Exception e) {
         onException(e);
     }
 
-    /**
-     * Logs the exception as an Error by default, but this method may
-     * be overridden by subclasses.
-     *
-     * @param e the exception thrown from {@link #onPreExecute()}, {@link #call()}, or {@link #onSuccess(Object)}
-     * @throws RuntimeException, ignored
-     */
     protected void onException(Exception e) throws RuntimeException {
         onThrowable(e);
     }
@@ -165,9 +115,7 @@ public abstract class SafeAsyncTask<ResultT> implements Callable<ResultT> {
         Log.e("SafeAsyncTask", "Throwable caught during background processing", t);
     }
 
-    /**
-     * @throws RuntimeException, ignored
-     */
+
     protected void onFinally() throws RuntimeException {
     }
 
@@ -274,14 +222,7 @@ public abstract class SafeAsyncTask<ResultT> implements Callable<ResultT> {
             });
         }
 
-        /**
-         * Posts the specified runnable to the UI thread using a handler,
-         * and waits for operation to finish.  If there's an exception,
-         * it captures it and rethrows it.
-         *
-         * @param c the callable to post
-         * @throws Exception on error
-         */
+
         protected void postToUiThreadAndWait(final Callable c) throws Exception {
             final CountDownLatch latch = new CountDownLatch(1);
             final Exception[] exceptions = new Exception[1];
